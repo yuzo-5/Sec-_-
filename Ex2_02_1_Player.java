b04ffe3c87f4673a588f6b4dd34228bea9e08a1f
yuzo-5 <t.horie.jpn@gmail.com>
2024-10-27 16:18:11 +0900

    initial commit

---
+++ Ex2_02_1_Player.java
@@ -0,0 +1,17 @@
+class EX2_02_1_Player {
+
+    //フィールド
+    String name ;  //プレイヤー名
+    String handStatus ;  //ジャンケンの手
+
+    //コンストラクタ
+    EX2_02_1_Player(String name){
+        n
+
+    }
+
+    //メソッド
+    void makeHandState(){
+        
+    }
+}
